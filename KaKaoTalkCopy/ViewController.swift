0245ecf6376160c1c7b927bdf3cf16204b3fbc40
alphachip <fsec1sun@gmail.com>
2020-06-01 13:55:00 +0900

    Initial Commit

---
+++ KaKaoTalkCopy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KaKaoTalkCopy
+//
+//  Created by dindon on 2020/06/01.
+//  Copyright © 2020 Alphachip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
